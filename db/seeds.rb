0f95f90b9e60fc055740e86819f1a462c9226101
Dave Rapin <dave@rapin.com>
2019-11-12 13:35:02 -0500

    Updated SSO targets to use samltest.id

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,8 @@ company = Company.create(
   name: "ACME Inc.",
   saml_settings: {
     idp_entity_id: "https://samltest.id/saml/idp",
-    idp_sso_target_url: "https://app.onelogin.com/trust/saml2/http-redirect/sso/onelogin_app_id",
-    idp_slo_target_url: "https://app.onelogin.com/trust/saml2/http-redirect/slo/onelogin_app_id"
+    idp_sso_target_url: "https://samltest.id/idp/profile/SAML2/Redirect/SSO",
+    idp_slo_target_url: "https://samltest.id/idp/profile/SAML2/Redirect/SLO"
   }
 )
 
